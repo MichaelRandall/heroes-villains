554cf8ddf8a7c82b244d94523750965d9b35b27b
m_ran66 <m_ran66@hotmail.com>
2017-05-19 18:34:32 -0700

    add http service to hero list controller to retrieve data from server

--- app/hero-list/hero-list.component.js
+++ app/hero-list/hero-list.component.js
@@ -5,35 +5,12 @@ angular.
 	module('heroList').
 		component('heroList', {
 			templateUrl: 'hero-list/hero-list.template.html',
-			controller: function HeroListController() {
-				this.heroes = [
-					{
-						hero_name:'Superman',
-						name: 'Clark Kent',
-						year: 1938
-					},{
-						hero_name:'Wonder Woman',
-						name: 'Diana Prince',
-						year: 1941
-					},{
-						hero_name:'Spider-Man',
-						name:'Peter Parker',
-						year: 1962
-					},{
-						hero_name:'Iron-Man',
-						name:'Tony Stark',
-						year: 1963
-					},{
-						hero_name:'Hulk',
-						name:'Bruce Banner',
-						year: 1962
-					},{
-						hero_name:'Batman',
-						name:'Bruce Wayne',
-						year: 1939
-					}
-				];
+			controller: function HeroListController($http) {
+				var self = this;
+				self.orderProp = 'year';
 
-				this.orderProp = 'year';
+				$http.get('heroes/heroes.json').then(function(response){
+					self.heroes = response.data;
+				});
 			}
 		});
